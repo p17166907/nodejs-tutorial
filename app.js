fc2c4ad3ef9947b01cc07fca9bfef660fe75e6bc
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-06-25 20:19:55 +0100

    test commit from mac

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const { readFileSync, writeFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require("fs");
 
+const first = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
-
-writeFileSync('./content/result-sync.txt',
-    `this is the result: ${first}, 
+writeFileSync(
+  "./content/result-sync.txt",
+  `this is the result: ${first}, 
     ${second}`,
-    { flag: 'w' })
-
+  { flag: "w" }
+);
