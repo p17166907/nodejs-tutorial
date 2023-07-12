8e9d7d4b3135e1d1d5e943dc58f1fd3054447a5d
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-07-12 09:55:12 +0100

    on methods POST jS example lesson

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 let { people } = require('./data.js')
 
+//parse json
+app.use(express.json())
+
+
 //static assets
 app.use(express.static('./methods-public'))
-//parse form data
+//parse form data from html file in static assests
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/people', (req, res) => { return res.status(200).json({ success: true, data: people }) })
@@ -13,6 +17,10 @@ app.post('/login', (req, res) => {
     const { name } = req.body
     if (name) { return res.status(200).send(`Welcome ${name}`) } else { return res.status(401).send(`Invalid!! Please provide a name`) }
 })
+
+app.post('/api/people', (req, res) => { 
+console.log(req.body);
+return res.status(201).send('Success') })
 
 
 app.all('*', (req, res) => { return res.status(404).send('<h1> Page not found</h1>') })
