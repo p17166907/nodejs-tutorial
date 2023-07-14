cd7925bb67c90ebc74cf055c927c2182f04f2af3
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-07-14 17:25:32 +0100

    finish PUT lesson, start  DELETE method lesson

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -18,9 +18,36 @@ app.post('/login', (req, res) => {
     if (name) { return res.status(200).send(`Welcome ${name}`) } else { return res.status(401).send(`Invalid!! Please provide a name`) }
 })
 
-app.post('/api/people', (req, res) => { 
-console.log(req.body);
-return res.status(201).send('Success') })
+app.post('/api/people', (req, res) => {
+    console.log(req.body);
+    const { name } = req.body
+    if (!name) { return res.status(400).json({ success: false, msg: 'please provide name value' }) }
+    return res.status(201).json({ success: true, person: name })
+})
+
+
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body
+    if (!name) { return res.status(400).json({ success: false, msg: 'please provide name value' }) }
+    return res.status(201).send({ success: true, data: [...people, name] })
+
+})
+
+app.put('/api/people/:id', (req, res) => {
+    //the id parameter is extracted from the request URL
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!name) { return res.status(400).json({ success: false, msg: 'Please provide a name' }); }
+
+    // Find the person by ID extracted from url and update their name
+    const person = people.find((person) => person.id === Number(id));     //parseInt - Converts a string to an integer.
+    if (!person) { return res.status(404).json({ success: false, msg: `no person with id: ${id}` }); }
+    person.name = name;
+
+    return res.status(200).json({ success: true, data: people });
+});
+
 
 
 app.all('*', (req, res) => { return res.status(404).send('<h1> Page not found</h1>') })
