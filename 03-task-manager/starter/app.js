baedc1b7ec1b92a6968ad2f7af6458c2c0e196c4
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-07-25 17:51:05 +0100

    setting up the .env file to hide credentials for db and connecting db

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,6 +5,12 @@ const app = express();
 // Import the routes for tasks from a separate file called 'tasks.js'
 const tasks = require('./routes/tasks');
 
+// Database connection
+let connectDB = require('./db/connect')
+//require the dot env file to be able to access its contents
+require('dotenv').config()
+
+
 // Middleware - Parse incoming JSON data in the request body
 app.use(express.json());
 
@@ -24,6 +30,22 @@ app.use('/api/v1/tasks', tasks);
 app.all('*', (req, res) => { res.status(404).send(`<h1>${res.statusCode} Page Not Found </h1>`); });
 
 const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}....`);
-});
+
+const start = async () => {
+    try {
+        //get credentials from the env file for our database
+        const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DBNAME } = process.env;
+        const connectionString = `mongodb+srv://${encodeURIComponent(
+            MONGO_USERNAME
+        )}:${encodeURIComponent(MONGO_PASSWORD)}@nodeexpressjstutorial.cyqm3ex.mongodb.net/${MONGO_DBNAME}?retryWrites=true&w=majority`;
+
+        await connectDB(connectionString);
+        app.listen(port, () => { console.log(`Server is listening on port ${port}....`); });
+
+    } catch (error) {
+        console.log('error from start async function app.js', error);
+
+    }
+}
+start()
+
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,1 +1,15 @@
-const connectionString = 'mongodb+srv://<username>:<password>@nodeexpressjstutorial.cyqm3ex.mongodb.net/?retryWrites=true&w=majority'
+
+
+// require mongoose to access db
+const mongoose = require('mongoose');
+
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    }).then(() => console.log('CONNECTED TO THE DB.....')).catch((error) => console.log(error));
+}
+
+module.exports = connectDB;
