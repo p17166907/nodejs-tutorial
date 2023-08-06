0367bc84c96304b407c96a6748ec0a821376fdd7
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-08-06 15:22:06 +0100

    request queries in products controller - getAllProducts

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,36 @@
-const  ProductModelSchema = require('../models/product')
+// Import the product model schema
+const ProductModelSchema = require('../models/product');
 
 const getAllProductsStatic = async (req, res, next) => {
-    const products = await ProductModelSchema.find({})
-    // throw new Error('testing async errors');
-    res.status(200).json({msg: 'Successs', data: products, nbHits: products.length});
-}
-const getAllProducts = async (req, res, next) => { res.status(200).send('Getting All Products'); }
+    try {
+        // Fetch all products
+        const products = await ProductModelSchema.find({});
+        res.status(200).json({ msg: 'Success', data: products, nbHits: products.length });
+    } catch (error) {
+        // Log errors specific to the static products retrieval
+        console.log('Error in getAllProductsStatic', error);
+    }
+};
 
-module.exports = { getAllProductsStatic, getAllProducts }
+const getAllProducts = async (req, res, next) => {
+    try {
+        const { featured, rating, createdAt, name, price, company } = req.query
+        const queryObject = {}
+        if (featured) { queryObject.featured = (featured === 'true') ? true : false }
+        // Use a case-insensitive regex for the name field if provided - allowing for more flexible matching.
+        if (company) { queryObject.company = { $regex: company, $options: 'i' }; }
+        // Use a case-insensitive regex for the name field if provided - allowing for more flexible matching.
+        if (name) { queryObject.name = { $regex: name, $options: 'i' }; }
+
+        console.log(queryObject);
+        // Fetch products based on the query object
+        const products = await ProductModelSchema.find(queryObject);
+        res.status(200).json({ msg: 'Success', data: products, nbHits: products.length });
+    } catch (error) {
+        // Log errors specific to the dynamic products retrieval
+        console.log('Error in getAllProducts', error);
+    }
+};
+
+// Export the functions
+module.exports = { getAllProductsStatic, getAllProducts };
