33c0930d7fff5e20a31516f1cae6f40a1f8f840d
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-08-06 20:15:33 +0100

    If a sort key is specified, apply sorting, If specific fields are specified, select only those fields to display on response

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,34 +1,51 @@
 // Import the product model schema
 const ProductModelSchema = require('../models/product');
 
-const getAllProductsStatic = async (req, res, next) => {
+// Function to retrieve all products from the database statically
+const getAllProductsStatic = async (req, res) => {
     try {
         // Fetch all products
         const products = await ProductModelSchema.find({});
+        // Send successful response with products data
         res.status(200).json({ msg: 'Success', data: products, nbHits: products.length });
     } catch (error) {
         // Log errors specific to the static products retrieval
-        console.log('Error in getAllProductsStatic', error);
+        console.error('Error in getAllProductsStatic', error);
     }
 };
 
-const getAllProducts = async (req, res, next) => {
+// Function to retrieve products based on provided query parameters
+const getAllProducts = async (req, res) => {
     try {
-        const { featured, rating, createdAt, name, price, company } = req.query
-        const queryObject = {}
-        if (featured) { queryObject.featured = (featured === 'true') ? true : false }
-        // Use a case-insensitive regex for the name field if provided - allowing for more flexible matching.
+        // Destructure query parameters
+        const { featured, company, name, sort, fields } = req.query;
+
+        // Prepare the query object based on available parameters
+        const queryObject = {};
+        if (featured) { queryObject.featured = featured === 'true'; }
+        // Use a case-insensitive regex for the company and name fields if provided - allowing for more flexible matching.
         if (company) { queryObject.company = { $regex: company, $options: 'i' }; }
-        // Use a case-insensitive regex for the name field if provided - allowing for more flexible matching.
         if (name) { queryObject.name = { $regex: name, $options: 'i' }; }
 
-        console.log(queryObject);
-        // Fetch products based on the query object
-        const products = await ProductModelSchema.find(queryObject);
+        // Begin the query to fetch products based on the query object 
+        let result = ProductModelSchema.find(queryObject);
+
+        // If a sort key is specified, apply sorting
+        if (sort) { result.sort(sort.split(',').join(' ')); } 
+        // Default sort by 'createdAt' if no sort key is provided
+        else { result.sort('createdAt'); }
+
+        // If specific fields are specified, select only those fields
+        if (fields) { result.select(fields.split(',').join(' ')); }
+
+        // Await the result after all modifications to the query
+        const products = await result;
+
+        // Send successful response with products data
         res.status(200).json({ msg: 'Success', data: products, nbHits: products.length });
     } catch (error) {
         // Log errors specific to the dynamic products retrieval
-        console.log('Error in getAllProducts', error);
+        console.error('Error in getAllProducts', error);
     }
 };
 
