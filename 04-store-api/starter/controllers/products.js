637174b4481e800fe419cf58c41ecee3edc6acc8
Evan Duru <evanduru@Evans-MacBook-Pro.local>
2023-08-07 13:01:36 +0100

    pagination - skip and limit user queries db schema methods in controller

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,10 +6,10 @@ const getAllProductsStatic = async (req, res) => {
     try {
         // Fetch all products
         const products = await ProductModelSchema.find({});
-        // Send successful response with products data
+        // Send a successful response with the products data
         res.status(200).json({ msg: 'Success', data: products, nbHits: products.length });
     } catch (error) {
-        // Log errors specific to the static products retrieval
+        // Log any errors that occur during the static retrieval of products
         console.error('Error in getAllProductsStatic', error);
     }
 };
@@ -17,34 +17,34 @@ const getAllProductsStatic = async (req, res) => {
 // Function to retrieve products based on provided query parameters
 const getAllProducts = async (req, res) => {
     try {
-        // Destructure query parameters
-        const { featured, company, name, sort, fields } = req.query;
+        // Destructure and extract required query parameters
+        const { featured, company, name, sort, fields, page = 1, limit = 10 } = req.query;
 
-        // Prepare the query object based on available parameters
+        // Prepare the query object based on the extracted parameters
         const queryObject = {};
-        if (featured) { queryObject.featured = featured === 'true'; }
-        // Use a case-insensitive regex for the company and name fields if provided - allowing for more flexible matching.
-        if (company) { queryObject.company = { $regex: company, $options: 'i' }; }
-        if (name) { queryObject.name = { $regex: name, $options: 'i' }; }
+        if (featured) { queryObject.featured = featured === 'true'; } // Convert 'featured' string to boolean
+        if (company) { queryObject.company = { $regex: company, $options: 'i' }; } // Case-insensitive matching
+        if (name) { queryObject.name = { $regex: name, $options: 'i' }; } // Case-insensitive matching
 
-        // Begin the query to fetch products based on the query object 
-        let result = ProductModelSchema.find(queryObject);
+        // Start constructing the query
+        let query = ProductModelSchema.find(queryObject);
 
-        // If a sort key is specified, apply sorting
-        if (sort) { result.sort(sort.split(',').join(' ')); } 
-        // Default sort by 'createdAt' if no sort key is provided
-        else { result.sort('createdAt'); }
+        // If 'sort' parameter is specified, apply sorting
+        if (sort) { query = query.sort(sort.split(',').join(' ')); }
+        // If 'fields' parameter is specified, select only those fields
+        if (fields) { query = query.select(fields.split(',').join(' ')); }
 
-        // If specific fields are specified, select only those fields
-        if (fields) { result.select(fields.split(',').join(' ')); }
+        // Apply pagination parameters
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(+limit);
 
-        // Await the result after all modifications to the query
-        const products = await result;
+        // Execute the query
+        const products = await query;
 
-        // Send successful response with products data
+        // Send a successful response with the products data
         res.status(200).json({ msg: 'Success', data: products, nbHits: products.length });
     } catch (error) {
-        // Log errors specific to the dynamic products retrieval
+        // Log any errors that occur during the dynamic retrieval of products
         console.error('Error in getAllProducts', error);
     }
 };
